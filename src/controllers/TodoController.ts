4b2d2f0a0e535cadd61d33c995f39c2177949ced
goni715 <halesgoni715149123@gmail.com>
2025-02-17 23:28:56 +0600

    update create todo

--- src/controllers/TodoController.ts
+++ src/controllers/TodoController.ts
@@ -4,14 +4,31 @@ import { db } from "../utils/dbConnect";
 
 export const createTodo = (req: Request, res:Response) => {
     const { name, email} = req.body;
-    const query = 'INSERT INTO todos (name, email) VALUES (?, ?) ';
-    db.query(query, [name, email], (err, results) => {
+
+   // Step 1: Check if email already exists
+  const checkEmailQuery = 'SELECT * FROM todos WHERE email = ?';
+  db.query(checkEmailQuery, [email], (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+
+    // If email exists, return an error
+    if (results.length > 0) {
+      return res.status(409).json({ message: 'Email already exists' });
+    }
+
+    // Step 2: Insert new 
+    const insertQuery = 'INSERT INTO todos (name, email) VALUES (?, ?)';
+    db.query(insertQuery, [name, email], (err, results) => {
       if (err) {
         return res.status(500).json({ error: err.message });
       }
-      res.status(201).json(results);
+      res.status(201).json({ id: results.insertId, name, email});
     });
+  });
 }
+
+
 
 
 export const getAllTodos = (req: Request, res:Response) => {
