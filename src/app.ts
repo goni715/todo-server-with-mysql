983411189335dedf4c1364d7edd808ff6fb3b2a5
goni715 <halesgoni715149123@gmail.com>
2025-02-17 10:14:42 +0600

    create config file

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,12 @@
-import express from 'express';
+import express, {Express, Request, Response} from 'express';
 
+const app: Express = express();
+
+
+app.get('/', (req: Request, res: Response)=> {
+    res.send('This is Todo Server with mysql database')
+})
+
+
+
+export default app;
